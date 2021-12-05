122db043888001c3a04835c7af6788ab355fa1ee
JCH <jchn0111@naver.com>
2021-12-05 18:01:54 +0900

    main

--- app/src/main/java/com/example/allrep/ui/community/fragment_commu/Fragment_community_make.java
+++ app/src/main/java/com/example/allrep/ui/community/fragment_commu/Fragment_community_make.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,18 +15,34 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.allrep.LoginViewModel;
 import com.example.allrep.R;
+import com.example.allrep.commuinfo.Commuinfo;
 import com.example.allrep.userinfo.Userinfo;
 import com.google.firebase.database.DatabaseReference;
 
 public class Fragment_community_make extends Fragment {
     DatabaseReference mDBReference = null;
     private LoginViewModel loginViewModel;
+    Button save;
     Userinfo user;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_community_make_text, container, false);
+        save = (Button)root.findViewById(R.id.make_text_button);
+        EditText title = (EditText)root.findViewById(R.id.writeheadtext);
+        EditText text = (EditText)root.findViewById(R.id.contentstext);
 
+
+
+
+        /*save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDBReference.child("/Commu_info/").get
+                Commuinfo commuinfo = new Commuinfo(user.userId, )
+            }
+        });*/
 
 
         return root;
