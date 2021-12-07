5368f623971cc0b7462b2342d6fb4dbbe4f9d991
JCH <jchn0111@naver.com>
2021-12-07 19:06:28 +0900

    커밋

---
+++ app/src/main/java/com/example/allrep/ui/home/feed/fragment/FeedingViewModelFactory.java
@@ -0,0 +1,20 @@
+package com.example.allrep.ui.home.feed.fragment;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class FeedingViewModelFactory implements ViewModelProvider.Factory {
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        try {
+            return modelClass.newInstance();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
