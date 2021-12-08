c56260b5f6a65a6483045866e0780244ee670090
Shiro <animetales@naver.com>
2021-12-08 20:27:07 +0900

    jun

--- app/src/main/java/com/example/allrep/ui/community/fragment_commu/Fragment_community_list.java
+++ app/src/main/java/com/example/allrep/ui/community/fragment_commu/Fragment_community_list.java
@@ -1,37 +1,61 @@
 package com.example.allrep.ui.community.fragment_commu;
 
+import android.app.LauncherActivity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.example.allrep.LoginViewModel;
 import com.example.allrep.R;
+import com.example.allrep.commuinfo.Commuinfo;
+import com.example.allrep.userinfo.Userinfo;
+import com.example.allrep.userinfo.communityinfo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Fragment_community_list extends Fragment {
     Fragment_community_make fragment_community_make;
     Fragment_community_text fragment_community_text;
+    DatabaseReference mDBReference = null;
+    private LoginViewModel loginViewModel;
+    Userinfo user;
+    List<Commuinfo> text_list;
+    Button make_text;
+    Button my_text;
+    Button combtn;
+    ListView listView;
+    Listitemadapter adapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_community_list_text, container, false);
         fragment_community_make = new Fragment_community_make();
         fragment_community_text = new Fragment_community_text();
-
-        Button make_text = (Button)root.findViewById(R.id.newcomunity);
-        Button combtn = (Button)root.findViewById(R.id.community_btn);
-
-        combtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getFragmentManager().beginTransaction().replace(R.id.community_container,fragment_community_text).commit();
-            }
-        });
-
+        make_text = (Button)root.findViewById(R.id.newcomunity);
+        my_text = (Button)root.findViewById(R.id.mycomunity);
         make_text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,6 +63,114 @@ public class Fragment_community_list extends Fragment {
             }
         });
 
+        listView = root.findViewById(R.id.community_listview);
+        adapter = new Listitemadapter();
+
+        mDBReference = FirebaseDatabase.getInstance().getReference();
+
+        mDBReference.child("/community_info/").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot data : snapshot.getChildren()) {
+                    String key = snapshot.getKey();
+                    HashMap<String, Object> writedata = (HashMap<String, Object>) data.getValue();
+                    //System.out.println(writedata);
+                    String[] getData = {writedata.get("number").toString(), writedata.get("userName").toString(), writedata.get("wirtetitle").toString()};
+                    //System.out.println(getData);
+                    String number = getData[0];
+                    int numberA = Integer.parseInt(number);
+                    String title = getData[1];
+                    String name = getData[2];
+                    adapter.addItem(numberA, title, name);
+                    adapter.notifyDataSetChanged();
+                }
+                listView.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int thrownumber = adapter.item.get(position).number;
+                Bundle bundle = new Bundle();
+                bundle.putInt("thrownumber",thrownumber);
+                fragment_community_text.setArguments(bundle);
+                getFragmentManager().beginTransaction().replace(R.id.community_container,fragment_community_text).commit();
+            }
+        });
+
         return root;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        loginViewModel = new ViewModelProvider(requireActivity()).get(LoginViewModel.class);
+        loginViewModel.getLoginedId().observe(getViewLifecycleOwner(), new Observer<Userinfo>() {
+            @Override
+            public void onChanged(Userinfo s) {
+                user = s;
+                make_text.setEnabled(true);
+                my_text.setEnabled(true);
+            }
+        });
+    }
+
+    public class Listitemadapter extends BaseAdapter{
+        ArrayList<communityinfo> item = new ArrayList<communityinfo>();
+        Context context;
+
+        @Override
+        public int getCount() {
+            return item.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return item.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Context context = parent.getContext();
+
+            if(convertView == null){
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.fragment_community_listitem,parent,false);
+            }
+            // 번호 : writenumberA 제목 : writetitleA 작성자 : writerA
+            TextView number = (TextView) convertView.findViewById(R.id.writenumberA);
+            TextView title = (TextView) convertView.findViewById(R.id.writetitleA);
+            TextView writer = (TextView) convertView.findViewById(R.id.writerA);
+
+            communityinfo items = (communityinfo) getItem(position);
+
+            number.setText(Integer.toString(items.getNumber()));
+            title.setText(items.getUserName());
+            writer.setText(items.getWirtetitle());
+
+            return convertView;
+        }
+        public void addItem(int number, String title, String writer){
+            communityinfo mItem = new communityinfo();
+
+            mItem.setNumber(number);
+            mItem.setWirtetitle(title);
+            mItem.setUserName(writer);
+
+            item.add(mItem);
+        }
+    }
 }
+
--- app/src/main/java/com/example/allrep/ui/community/fragment_commu/Fragment_community_text.java
+++ app/src/main/java/com/example/allrep/ui/community/fragment_commu/Fragment_community_text.java
@@ -1,22 +1,185 @@
 package com.example.allrep.ui.community.fragment_commu;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.example.allrep.LoginViewModel;
 import com.example.allrep.R;
+import com.example.allrep.commuinfo.Commeinfo;
+import com.example.allrep.userinfo.Userinfo;
+import com.example.allrep.userinfo.communityinfo;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Fragment_community_text extends Fragment {
+    DatabaseReference mDBReference = null;
+    private LoginViewModel loginViewModel;
+    Fragment_community_make fragment_community_make;
+    Fragment_community_list fragment_community_list;
+    Userinfo user;
+    String name;
+    Commeinfo commeInfo = null;
+    Comment_adapter ca;
+    int numberdata = 0;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mDBReference = FirebaseDatabase.getInstance().getReference();
         View root = inflater.inflate(R.layout.fragment_community_text, container, false);
+        Button remove_btn = (Button)root.findViewById(R.id.remove_text);
+        Button remake_btn = (Button)root.findViewById(R.id.remake_text);
+        TextView text_title = (TextView)root.findViewById(R.id.text_title);
+        TextView text_user_name = (TextView)root.findViewById(R.id.text_user_name);
+        TextView community_text = (TextView)root.findViewById(R.id.community_text);
+        TextView text_no = (TextView)root.findViewById(R.id.text_num);
+        EditText comme_text = (EditText)root.findViewById(R.id.comme_text);
+        Button comme_text_btn = (Button)root.findViewById(R.id.comme_text_btn);
+        ListView comment_list = (ListView) root.findViewById(R.id.comment_list);
+        ca = new Comment_adapter();
+
+
+
+        int numberA = getArguments().getInt("thrownumber");
+
+        // DB내용 number userName wirtetitle writecontent
+        mDBReference.child("/community_info/").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot DBdata : snapshot.getChildren()) {
+                    String key = DBdata.getKey();
+                    HashMap<String, Object> cummdata = (HashMap<String, Object>) DBdata.getValue();
+                    String[] getData = {cummdata.get("number").toString(), cummdata.get("userName").toString(), cummdata.get("wirtetitle").toString(), cummdata.get("writecontent").toString()};
+                    String number = getData[0];
+                    int numberB = Integer.parseInt(number);
+                    name = getData[1];
+                    String title = getData[2];
+                    String content = getData[3];
+                    //System.out.println(user.userId);
+                    //System.out.println(name);
+                    //System.out.println(numberA);
+                    //System.out.println(numberB);
+
+                    // text_title 타이틀 text_user_name 유저네임 community_text 내용 text_num 텍스트번호
+                    if (numberA == numberB){
+                        text_title.setText(title);
+                        text_user_name.setText(name);
+                        community_text.setText(content);
+                        text_no.setText(Integer.toString(numberB));
+                        if (user.userId.equals(name)){
+                            remove_btn.setEnabled(true);
+                            remake_btn.setEnabled(true);
+                        }
+                        break;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        mDBReference.child("/Comme_info/").child(Integer.toString(numberA)).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot data : snapshot.getChildren()) {
+                    String key = data.getKey();
+                    HashMap<String, Object> commeinfo = (HashMap<String, Object>) data.getValue();
+                    String[] getdata = {commeinfo.get("number").toString(), commeinfo.get("userId").toString(), commeinfo.get("commetext").toString(), commeinfo.get("num").toString()};
+                    if(numberA == Integer.parseInt(getdata[0])){
+                        Commeinfo com = new Commeinfo(Integer.parseInt(getdata[0]), Integer.parseInt(getdata[3]), getdata[1], getdata[2]);
+                        ca.item.add(com);
+                    }
+                }
+                comment_list.setAdapter(ca);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        mDBReference.child("/Comme_info/").child(Integer.toString(numberA)).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot data : snapshot.getChildren()) {
+                    String key = data.getKey();
+                    HashMap<String, Object> userInfo = (HashMap<String, Object>) data.getValue();
+                    String getdata = userInfo.get("num").toString();
+                    int number = Integer.parseInt(getdata);
+                    if (numberdata < number ){
+                        numberdata = number;
+                    }
+                    System.out.println(numberdata);
+                }
+                System.out.println(numberdata);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        comme_text_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view2) {
+
+
+                String CommeText = comme_text.getText().toString();
+                numberdata++;
+                commeInfo = new Commeinfo(numberA, numberdata, user.userId, CommeText);
+
+                mDBReference.child("/Comme_info/").child(Integer.toString(numberA)).child(Integer.toString(numberdata)).setValue(commeInfo).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid2) {
+                        System.out.println(numberdata);
+                        Toast.makeText(root.getContext(), "작성완료", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        loginViewModel = new ViewModelProvider(requireActivity()).get(LoginViewModel.class);
+        loginViewModel.getLoginedId().observe(getViewLifecycleOwner(), new Observer<Userinfo>() {
+            @Override
+            public void onChanged(Userinfo s) {
+                user = s;
+            }
+        });
     }
 }
