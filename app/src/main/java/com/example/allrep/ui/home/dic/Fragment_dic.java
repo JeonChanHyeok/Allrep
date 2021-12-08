6add72769f7340ba86aa82508deec2a3246366f2
lanoon28 <lanoon28@gmail.com>
2021-12-08 17:33:26 +0900

    어댑터3

--- app/src/main/java/com/example/allrep/ui/home/dic/Fragment_dic.java
+++ app/src/main/java/com/example/allrep/ui/home/dic/Fragment_dic.java
@@ -1,9 +1,11 @@
 package com.example.allrep.ui.home.dic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -12,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.allrep.R;
+import com.example.allrep.ui.home.feed.Feeding_helper;
 import com.example.allrep.userinfo.Animalinfo;
 import com.example.allrep.userinfo.Dicinfo;
 import com.google.firebase.database.DataSnapshot;
@@ -25,11 +28,14 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class Fragment_dic extends Fragment {
-    Fragment_dic_animal_info fragment_dic_animal_info;
+    Fragment_dic fragment_dic;
     private ListView mListView;
     DatabaseReference mDBReference = null;
     Fragment_dic_adapter1 adapter;
     ArrayList<String> category;
+    String middle_title;
+    String small_title;
+    int page;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -39,27 +45,100 @@ public class Fragment_dic extends Fragment {
         Fragment_dic_adapter1 adapter = new Fragment_dic_adapter1();
         mListView = (ListView) rootView.findViewById(R.id.diction_category);
         Button btn = (Button)rootView.findViewById(R.id.dic_button);
-        fragment_dic_animal_info = new Fragment_dic_animal_info();
+        fragment_dic = new Fragment_dic();
         category = new ArrayList<String>();
-        mDBReference.child("/Dic_info/").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot d : snapshot.getChildren()) {
-                    category.add(d.getKey());
+        page = 0;
+        page = getArguments().getInt("page");
+        middle_title = getArguments().getString("middle_title");
+        small_title = getArguments().getString("small_title");
+
+        if(page == 0 ){
+            mDBReference.child("/Dic_info/").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot d : snapshot.getChildren()) {
+                        category.add(d.getKey());
+                    }
+                    for(int i = 0; i<category.size();i++){
+                        adapter.mItems.add(category.get(i));
+                    }
+                    mListView.setAdapter(adapter);
                 }
-                for(int i = 0; i<category.size();i++){
-                    adapter.mItems.add(category.get(i));
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
                 }
-                mListView.setAdapter(adapter);
+            });
+        }
+        else if (page == 1){
+            mDBReference.child("/Dic_info/").child(middle_title).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot d : snapshot.getChildren()) {
+                        category.add(d.getKey());
+                    }
+                    for(int i = 0; i<category.size();i++){
+                        adapter.mItems.add(category.get(i));
+                    }
+                    mListView.setAdapter(adapter);
 
 
 
 
-            }
+                }
 
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+        else if (page == 2){
+            mDBReference.child("/Dic_info/").child(middle_title).child(small_title).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot d : snapshot.getChildren()) {
+                        category.add(d.getKey());
+                    }
+                    for(int i = 0; i<category.size();i++){
+                        adapter.mItems.add(category.get(i));
+                    }
+                    mListView.setAdapter(adapter);
+
+
+
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+
+        mListView.setOnItemClickListener(new ListView.OnItemClickListener() {
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(page==0){
+                   middle_title = adapter.mItems.get(i);
+                   Bundle bundle = new Bundle();
+                   bundle.putString("middle_title",middle_title);
+                   bundle.putString("small_title","");
+                   bundle.putInt("page",1);
+                   fragment_dic.setArguments(bundle);
+                   getFragmentManager().beginTransaction().replace(R.id.container,fragment_dic).commit();
+                }
+                else if(page ==1){
+                    small_title = adapter.mItems.get(i);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("middle_title",middle_title);
+                    bundle.putString("small_title",small_title);
+                    bundle.putInt("page",2);
+                    fragment_dic.setArguments(bundle);
+                    getFragmentManager().beginTransaction().replace(R.id.container,fragment_dic).commit();
 
+                }
             }
         });
 
--- app/src/main/java/com/example/allrep/ui/home/dic/Fragment_dic_adapter3.java
+++
@@ -1,4 +0,0 @@
-package com.example.allrep.ui.home.dic;
-
-public class Fragment_dic_adapter3 {
-}
